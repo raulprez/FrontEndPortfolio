623508243547d2308b71e7bbdda41028d1bafeaa
raulprez <usernamerpc@gmail.com>
2023-08-06 00:33:24 -0600

    Add logo and updated dark mode toggle, add resume button

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,4 +1,7 @@
 import React, { useState, useEffect } from 'react';
+import { DarkModeSwitch } from 'react-toggle-dark-mode';
+
+
 
 function NavBar() {
   const [darkToggle, setDarkToggle] = useState(false);
@@ -15,20 +18,26 @@ function NavBar() {
   }, [darkToggle]);
 
   return (
-    <main className={`h-10 w-full bg-white dark:bg-gray-800 flex align-middle p-5 ${darkToggle ? 'dark' : ''}`}>
-      <label class="toggleDarkBtn">
-        <span class="slideBtnTg round"></span>
-        <div>
-          <input
-            className="mr-2 mt-[0.3rem] h-3.5 w-8 appearance-none rounded-[0.4375rem] bg-neutral-300 before:pointer-events-none before:absolute before:h-3.5 before:w-3.5 before:rounded-full before:bg-transparent before:content-[''] after:absolute after:z-[2] after:-mt-[0.1875rem] after:h-5 after:w-5 after:rounded-full after:border-none after:bg-neutral-100 after:shadow-[0_0px_3px_0_rgb(0_0_0_/_7%),_0_2px_2px_0_rgb(0_0_0_/_4%)] after:transition-[background-color_0.2s,transform_0.2s] after:content-[''] checked:bg-primary checked:after:absolute checked:after:z-[2] checked:after:-mt-[3px] checked:after:ml-[1.0625rem] checked:after:h-5 checked:after:w-5 checked:after:rounded-full checked:after:border-none checked:after:bg-primary checked:after:shadow-[0_3px_1px_-2px_rgba(0,0,0,0.2),_0_2px_2px_0_rgba(0,0,0,0.14),_0_1px_5px_0_rgba(0,0,0,0.12)] checked:after:transition-[background-color_0.2s,transform_0.2s] checked:after:content-[''] hover:cursor-pointer focus:outline-none focus:ring-0 focus:before:scale-100 focus:before:opacity-[0.12] focus:before:shadow-[3px_-1px_0px_13px_rgba(0,0,0,0.6)] focus:before:transition-[box-shadow_0.2s,transform_0.2s] focus:after:absolute focus:after:z-[1] focus:after:block focus:after:h-5 focus:after:w-5 focus:after:rounded-full focus:after:content-[''] checked:focus:border-primary checked:focus:bg-primary checked:focus:before:ml-[1.0625rem] checked:focus:before:scale-100 checked:focus:before:shadow-[3px_-1px_0px_13px_#3b71ca] checked:focus:before:transition-[box-shadow_0.2s,transform_0.2s] dark:bg-teal-800 dark:after:bg-teal-500 dark:checked:bg-primary dark:checked:after:bg-primary dark:focus:before:shadow-[3px_-1px_0px_13px_rgba(255,255,255,0.4)] dark:checked:focus:before:shadow-[3px_-1px_0px_13px_#3b71ca]"
-            type="checkbox" onClick={() => setDarkToggle(!darkToggle)}
+    <nav className={`h-10 w-full bg-white dark:bg-gray-800 p-5 ${darkToggle ? 'dark' : ''}`}>
+      <ul className='flex items-center justify-between lg:px-8'>
+        <li>
+        <img
+            src={darkToggle ? "/LbWhite.png" : "/LbBlack.png"} 
+            alt="Logo"
+            className="h-12" 
           />
-          <label className="text-black dark:text-white" onClick={() => setDarkToggle(!darkToggle)}>
-            {checkBoxText} {/* Use the state value here */}
-          </label>
-        </div>
-      </label>
-    </main>
+        </li>
+        <li className='flex flex-col align-middle'>
+            <DarkModeSwitch checked={darkToggle} onChange={setDarkToggle} size={30}/>
+              <label className="text-black dark:text-white" onClick={() => setDarkToggle(!darkToggle)}>
+                {checkBoxText} 
+              </label>
+        </li>
+        <li>
+          <a className="bg-teal-500 text-white px-4 py-1.5 border-none rounded-md ml-8" href="#"> Resume </a>
+        </li>
+      </ul>
+    </nav>
   );
 }
 
